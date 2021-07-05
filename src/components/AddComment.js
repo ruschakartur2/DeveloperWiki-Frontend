f356dd9abd1cc3515b8a4f0decb0316d8dbb4dbe
Artur <arturruschak1@gmail.com>
2021-07-05 15:49:37 +0300

    Update AddComment.js

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,6 +1,5 @@
 import React, {useRef, useState} from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
@@ -19,7 +18,6 @@ const required = (value) => {
 };
 
 const AddComment = (props) => {
-    const { user: currentUser } = useSelector((state) => state.auth);
     const { isLoggedIn } = useSelector(state => state.auth);
 
     const dispatch = useDispatch();
@@ -29,7 +27,6 @@ const AddComment = (props) => {
     const form = useRef();
 
     const [content,setContent] = useState("");
-    const [submitted, setSubmitted] = useState(false);
 
 
     const onChangeContent = (e) => {
@@ -69,9 +66,7 @@ const AddComment = (props) => {
 
                 <div className="form-group">
                     <button className="btn btn-primary btn-block" disabled={submitted}>
-                        {submitted && (
-                            <span className="spinner-border spinner-border-sm"></span>
-                        )}
+                      
                         <span>Comment</span>
                     </button>
                 </div>
