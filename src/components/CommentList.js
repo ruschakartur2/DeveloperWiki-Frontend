874380112e4b1bfb3829b9a88fc0f325120b8f5d
Artur <arturruschak1@gmail.com>
2021-07-05 15:46:44 +0300

    Update CommentList.js

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {deleteComment} from '../actions/comments';
