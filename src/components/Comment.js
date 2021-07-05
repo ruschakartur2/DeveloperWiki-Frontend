eae49789a3ce42bdfa742920269bc5060cd97579
Artur <arturruschak1@gmail.com>
2021-07-05 16:00:32 +0300

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {deleteComment} from '../actions/comments';
 
