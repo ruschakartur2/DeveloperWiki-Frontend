fa4766bc27235c426776ac10d00a22be48cdda02
Artur <arturruschak1@gmail.com>
2021-07-06 12:07:16 +0300

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USERS_URL = "https://swiki.bvblogic.dev/api/v1/accounts/;
+const USERS_URL = "https://swiki.bvblogic.dev/api/v1/accounts/";
 
 const register = (email, password) => {
   return axios.post(USERS_URL + "create/", {
