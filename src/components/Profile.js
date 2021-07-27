591c52e446868c31c65ae9f73992ecddd3e8d4e7
ruschakartur2 <ruschakartur@gmail.com>
2021-07-27 16:06:09 +0300

    Fix warnings

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,10 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import UpdateProfile from "./UpdateProfile";
 import {getProfile} from "../actions/auth";
 import {Redirect} from "react-router-dom";
 
 
 const Profile = () => {
-    const profile = useSelector(state => state.auth.profile);
     const user = useSelector(state => state.auth.user);
     const dispatch = useDispatch();
 
